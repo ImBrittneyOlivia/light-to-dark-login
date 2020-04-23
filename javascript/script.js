3318226613cba2b7b4c802575925cebc10e6a0f3
Bri <brittneystuart2613@gmail.com>
2020-04-22 23:32:09 -0700

    button dark/light txt toggles

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,19 @@
-function dakMode() {
-  let change = document.body;
-  change.classList.toggle("darkMode");
-}
+// function dakMode() {
+//   let change = document.body;
+//   change.classList.toggle("darkMode");
+// }
 
 function myFunction() {
+  let buttonTxt = document.getElementById("toggleBtnTxt");
+
   var element = document.body;
   element.classList.toggle("darkMode");
+
+  if (buttonTxt.innerHTML === "Light Mode") {
+    buttonTxt.innerHTML = "Dark Mode";
+  } else {
+    buttonTxt.innerHTML = "Light Mode";
+  }
 }
 
 // dynamic copyright date
